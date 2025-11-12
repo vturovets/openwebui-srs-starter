67cec441ac58d01d26183370bef70dbd15c36673
vturovets <vturovets@gmail.com>
2025-11-12 14:23:17 +0200

    Revert "Add import manager with FastAPI endpoints and tests"

--- backend/app/imports/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Import job orchestration package."""
-
-from .manager import ImportManager, ImportStatusSnapshot, ImportSummary
-
-__all__ = ["ImportManager", "ImportStatusSnapshot", "ImportSummary"]
-
