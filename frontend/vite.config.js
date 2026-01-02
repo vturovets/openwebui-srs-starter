4c51edf00f31b02029167d8dbf196ff4ab7a14f1
vturovets <vturovets@gmail.com>
2026-01-02 13:38:05 +0200

    Revert "Add env-controlled console visibility options"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
-  envDir: '..',
   server: {
     port: 4173,
   },
@@ -14,3 +13,4 @@ export default defineConfig({
     exclude: ['**/node_modules/**', '**/dist/**', 'tests/**'],
   },
 });
+
